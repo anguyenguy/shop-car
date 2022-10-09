7bbbbb2cb90cdf8cc43ee80b2c13b81d6c8e2474
quannguyen <hdragon1501@gmail.com>
2022-10-09 22:47:22 +0700

    Fix bug

--- source/js/myscript.js
+++ source/js/myscript.js
@@ -14,3 +14,24 @@ $.each(data, function(index, value) {
 	console.log(index + ': ' + value.name);
 	console.log(index + ': ' + value.age);
 });
+
+
+	$(function () {
+  $("#mydd a").on('click',function (e) {
+  	e.preventDefault();
+    $("#dropdownMenu1").html($(this).html() + ' <span class="downicon"></span>');
+  });
+});
+
+$(function () {
+  $("#mydd2 a").on('click',function (e) {
+  	e.preventDefault();
+    $("#dropdownMenu2").html($(this).html() + ' <span class="downicon"></span>');
+  });
+});
+$(function () {
+  $("#mydd3 a").on('click',function (e) {
+  	e.preventDefault();
+    $("#dropdownMenu3").html($(this).html() + ' <span class="downicon"></span>');
+  });
+});
